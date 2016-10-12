ce77f458011a9fb25a2c6124adf12f73914e7914
Julien BRAT <bratjulien@gmail.com>
2016-10-12 21:02:34 +0200

    init spring boot project with simple controller

---
+++ src/main/java/com/socialNetwork/controllers/HomeController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.socialNetwork.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Main controller of the application
+ * 
+ * @author UJM student's
+ */
+@Controller
+public class HomeController {
+    
+    /**
+     * Method to access to the home page of the application 
+     * 
+     * @param model for thymeleaf
+     * 
+     * @return template
+     */
+    @RequestMapping("/home") 
+    public String home(Model model) {
+        return "index";
+    }
+    
+    @RequestMapping("/test")
+    public String testStatement() {
+        return "index";
+    }
+}
