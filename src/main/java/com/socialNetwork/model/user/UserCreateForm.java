c9d4a6ac185e4367b984e828fbb8b380c649fe0d
Julien BRAT <bratjulien@gmail.com>
2016-11-07 16:18:33 +0100

    delete databases

--- src/main/java/com/socialNetwork/model/user/UserCreateForm.java
+++ src/main/java/com/socialNetwork/model/user/UserCreateForm.java
@@ -28,30 +28,66 @@ public class UserCreateForm {
         return login;
     }
 
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getPasswordRepeated() {
         return passwordRepeated;
+    }
+
+    public void setPasswordRepeated(String passwordRepeated) {
+        this.passwordRepeated = passwordRepeated;
     }
 
     public UserRole getRole() {
         return role;
     }
-    
-    
 
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "UserCreateForm{" +
+            "email='" + email.replaceFirst("@.+", "@***") + '\'' +
+            ", password=***" + '\'' +
+            ", passwordRepeated=***" + '\'' +
+            ", role=" + role +
+            '}';
+    }
 }
