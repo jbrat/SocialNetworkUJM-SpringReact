3bc54c50bba1c59625e32e79a4082bb26f249963
Julien BRAT <bratjulien@gmail.com>
2016-11-30 11:36:54 +0100

    refactor, indentation, comments all exceptions

--- src/main/java/com/socialNetwork/exception/EmailAlreadyExistException.java
+++ src/main/java/com/socialNetwork/exception/EmailAlreadyExistException.java
@@ -1,7 +1,8 @@
 package com.socialNetwork.exception;
 
 /**
- * Class to Email Exception
+ * Class to Email Exception, this exception is throws when the user try to register an email which is allready use by other user
+ * 
  * @author UJM's students
  */
 public class EmailAlreadyExistException extends Exception {
--- src/main/java/com/socialNetwork/exception/PasswordNotMatchException.java
+++ src/main/java/com/socialNetwork/exception/PasswordNotMatchException.java
@@ -1,7 +1,8 @@
 package com.socialNetwork.exception;
 
 /**
- * Class to Password Exception
+ * Class to Password Exception, this exception is throw when the twho password in form register didn't match
+ * 
  * @author UJM's students
  */
 public class PasswordNotMatchException extends Exception {
